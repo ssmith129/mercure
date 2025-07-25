b7706e68fbabc6b3c37aa355267b621044e712f0
ssmith129 <111539897+ssmith129@users.noreply.github.com>
2025-07-25 14:06:01 -0700

    Improve dropdown hover behavior to stay open while navigating

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -43,7 +43,7 @@ const Header = () => {
               </button>
               
               {activeDropdown === 'issues' && (
-                <div className="absolute top-full left-0 mt-2 w-64 bg-white rounded-lg shadow-lg py-2 z-50">
+                <div className="absolute top-full left-0 mt-1 w-64 bg-white rounded-lg shadow-lg py-2 z-50">
                   {coreIssues.map((issue, index) => (
                     <a
                       key={index}
